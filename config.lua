6763b172bfb90b25456564ba4492ec8d86aaa0ab
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2022-10-05 02:53:04 +0200

    V1

---
+++ config.lua
@@ -0,0 +1,10 @@
+Boxing = {
+    {x=-1274.7,y=-1530.9,z=5.2,h=258.0,type=25,distance=4.0,size=7.0, r=255, g=0, b=0,text="Appuie sur ~INPUT_PICKUP~ pour prendre/rendre les ~b~gants de boxe"},
+}
+
+-- Système de gants de boxe avec différents points d'activation (idle 0.0 ms)
+
+-- Tools :
+-- {x=,y=,z=,h=,type=,distance=,size=, r=, g=, b=,text=""},
+-- Ici "type" est l'index de drawmarker, "distance" est la distance d'activation et "size" est la taille de de drawmarker
+-- Surtout ne pas oublier d'instalelr ox_lib pour que cela fonctionne
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+Author 'ChernyyOrel#1074'
+game 'gta5'
+lua54 'yes'
+
+client_script {
+    'client/**.lua',
+}
+
+shared_script {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
